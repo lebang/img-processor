daa0033d908ad8330beb0c0fb4fa70c96f64e7d9
ethanbli <ethanbli@tencent.com>
2025-10-13 19:37:13 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import inquirer from 'inquirer';
+import { fileSelector } from 'inquirer-file-selector';
+
+const askForOption = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: '选择操作?',
+            choices: [
+                {
+                    name: '生成pdf',
+                    value: 'pdf',
+                },
+                {
+                    name: '生成最终客户文件夹',
+                    value: 'prod',
+                },
+            ],
+        },
+    ]);
+};
+
+const askForFolder = () => {
+    return fileSelector({
+        message: '请选择目录（回车确认）：',
+        root: process.cwd(),
+        selectionType: 'folder',
+    });
+};
+
+const handleErrors = (error) => {
+    if (error.message.includes('User force closed the prompt')) {
+        console.log('Prompt was canceled.');
+    } else {
+        console.error('An error occurred:', error);
+    }
+};
+
+async function main() {
+    try {
+        const optionAnswer = await askForOption();
+        
+        let folderAnswer;
+        do {
+            folderAnswer = await askForFolder();
+            if (!folderAnswer.isDirectory) {
+                console.log('\n您选择的不是一个目录，请重新选择。\n');
+            }
+        } while (!folderAnswer.isDirectory);
+
+        console.log(`选择的操作是： ${optionAnswer.option}，选择的目录是： ${folderAnswer.path}`);
+    } catch (error) {
+      handleErrors(error);
+    }
+}
+
+main();
